691221e59139addecb1afc190bddece40a366994
Hossam Marey <hossam.marey@rytalo.com>
2021-08-17 13:23:43 +0200

    first

--- components/seo/MainHead.js
+++ components/seo/MainHead.js
@@ -22,8 +22,6 @@ const mainHead = ({ title, description, keywords, icon }) => {
 
       <link rel="icon" href={icon ? icon : "/images/faviconWhite.png"} />
 
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
         rel="stylesheet"
